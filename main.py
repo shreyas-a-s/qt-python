f865dfa9cc62b901353342d1449871e8fd6cb99a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-21 00:37:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from WelcomeDialog import *
+from PyQt5.QtWidgets import QApplication, QDialog
+
+class WelcomeDialog(QDialog, Ui_WelcomeDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    welcome_dialog = WelcomeDialog()
+    welcome_dialog.show()
+    sys.exit(app.exec_())
+
