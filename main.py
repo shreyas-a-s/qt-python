e63ac3685a9dfb1d1a31786d2b4ed4b8f300bbab
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-21 21:34:38 +0530

    Simplify execution of WelcomeDialog.py from main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 
-from WelcomeDialog import *
-from PyQt5.QtWidgets import QApplication, QDialog
+import subprocess
 
-class WelcomeDialog(QDialog, Ui_WelcomeDialogWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    welcome_dialog = WelcomeDialog()
-    welcome_dialog.show()
-    sys.exit(app.exec_())
+subprocess.run(["python3", "WelcomeDialog.py"])
 
